67e5026b527752727007390aa2c5bcc038928670
Alisson <alisson.oliveira@ucsal.edu.br>
2023-12-03 23:31:10 -0300

    Projeto

---
+++ src/main/java/br/ucsal/tcc/avatarizer/AvatarizerApplication.java
@@ -0,0 +1,13 @@
+package br.ucsal.tcc.avatarizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvatarizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvatarizerApplication.class, args);
+	}
+
+}
